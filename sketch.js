94c678d10e1a2929b54d230d53a069a2dc7f3f74
cyberpunk3011 <aryanemenon1403@gmail.com>
2021-01-18 16:56:17 +0530

    Aryan

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
-var engine, world;
-var box1, pig1;
+var engine, world
+var box1, pig1
 var backgroundImg,platform;
 
 function preload() {
